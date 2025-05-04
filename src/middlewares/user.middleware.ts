7de3efb6c4b9d7832f520255291fd97c9a9cdf4c
SimplyAditya <adityabansal200311@gmail.com>
2025-05-05 04:01:41 +0530

    feat: Add nodemailer dependency and enhance user middleware error handling

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -1,18 +1,27 @@
 import { NextFunction, Request, Response } from "express";
 import { decodeToken } from "../utilities/commonFunctions";
+import { getUserById } from "../repositories/user.repository";
 
 export const userMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
-  const authorization = req.headers["authorization"];
-  if (!authorization) {
-    res.status(401).json({ message: "Unauthorized" });
-    return;
+  try{
+    const authorization = req.headers["authorization"];
+    if (!authorization) {
+      throw new Error("Authorization header is missing");
+    }
+    const token = authorization?.split(" ")[1];
+    const decodedToken = await decodeToken(token);
+    const user = await getUserById(decodedToken.userId);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    req.body = { ...req.body, userId: decodedToken.userId, role: user.role };
+    next();
+  }catch (error) {
+    console.error("Error in user middleware:", error.message);
+    res.status(500).json({ message: error.message });
   }
-  const token = authorization?.split(" ")[1];
-  const decodedToken = await decodeToken(token);
-  req.body = { ...req.body, userId: decodedToken.userId };
-  next();
 };
