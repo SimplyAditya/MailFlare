7c8b07e6547a817cada210053144cb2231ec767b
SimplyAditya <adityabansal200311@gmail.com>
2025-05-04 14:18:21 +0530

    chore: added scripts in package.json

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+    res.send('<html><style>.text{color:red; display: flex; justify-content: center; align-items: center; height: 100vh; font-size: 3rem}</style><body><div class="text">404! Page Not Found</div></body></html>');
 });
-app.listen(port, () => {
-    return console.log(`Express is listening at http://localhost:${port}`);
+app.listen(port, (err) => {
+    if (err) {
+        console.error("Error starting server:", err);
+        return;
+    }
+    console.log(`Express is listening at http://localhost:${port}`);
 });
+//# sourceMappingURL=index.js.map
